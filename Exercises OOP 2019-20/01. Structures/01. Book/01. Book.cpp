1c87718533f76ecb1d4ca727315b79784e0033e1
ekov1 <kistian_ekov@abv.bg>
2021-03-11 22:21:56 +0200

    .

---
+++ Exercises OOP 2019-20/01. Structures/01. Book/01. Book.cpp
@@ -0,0 +1,111 @@
+﻿//Да се напише структура "Book", която съдържа следната информация(полета) :
+//
+//    име на книгата
+//    име на автора
+//    жанр
+//    цена
+//    брой продажби
+//    други по ваш избор
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+const int SIZE = 100;
+
+struct Book {
+	char title[SIZE];
+	char author[SIZE];
+	char genre[SIZE];
+	double price;
+	int sales;
+};
+
+//Напишете функция, която прочита въведени стойности 
+//и ги задава като стойности на полетата на подаден обект от горния тип.
+void Initialize(Book& b) {
+	cout << "Title: " << endl;
+	cin.sync();
+	cin.getline(b.title, SIZE);
+
+	cout << "Author: " << endl;
+	cin.sync();
+	cin.getline(b.author, SIZE);
+
+	cout << "Genre: " << endl;
+	cin.sync();
+	cin.getline(b.genre, SIZE);
+
+	cout << "Price: " << endl;
+	cin.sync();
+	cin >> b.price;
+
+	cout << "Sales: " << endl;
+	cin.sync();
+	cin >> b.sales;
+	cin.ignore();
+}
+
+// Напишете функция, която отпечатва информацията от даден обект горния тип.
+void Print(const Book& b) {
+	cout << "Title: " << b.title << endl;
+	cout << "Author: " << b.author << endl;
+	cout << "Genre: " << b.genre << endl;
+	cout << "Price: " << b.price << endl;
+	cout << "Sales: " << b.sales << endl;
+}
+
+void PrintBookWiththeLowestPrice(const Book* books, int booksCount) {
+	int lp = books[0].price;
+	int bestBookId = 0;
+
+	for (int i = 0; i < booksCount; ++i)
+	{
+		if (books[i].price < lp)
+		{
+			lp = books[i].price;
+			bestBookId = i;
+		}
+	}
+
+	Print(books[bestBookId]);
+}
+
+Book GetBestSellingBook(const Book* books, int booksCount) {
+	int ms = books[0].price;
+	int bestBookId = 0;
+
+	for (int i = 0; i < booksCount; ++i)
+	{
+		if (books[i].sales > ms)
+		{
+			ms = books[i].sales;
+			bestBookId = i;
+		}
+	}
+
+	return books[bestBookId];
+}
+
+int main()
+{
+	const int booksCount = 3;
+	Book books[booksCount];
+
+	for (int i = 0; i < booksCount; i++)
+	{
+		Initialize(books[i]);
+	}
+
+	for (int i = 0; i < booksCount; i++)
+	{
+		Print(books[i]);
+	}
+
+	PrintBookWiththeLowestPrice(books, booksCount);
+
+	Book bestBook = GetBestSellingBook(books, booksCount);
+
+	Print(bestBook);
+
+}
---
+++ Exercises OOP 2019-20/01. Structures/02. DynArr/02. DynArr.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+
+struct DynArray {
+	int* data;
+	int size;
+	int capacity;
+};
+
+DynArray Create(int initialCapacity = 2) {
+	DynArray da;
+	da.capacity = initialCapacity;
+	da.size = 0;
+	da.data = new int[initialCapacity];
+	return da;
+}
+
+void Resize(DynArray& da) {
+	da.capacity *= 2;
+	int* oldData = da.data;
+	da.data = new int[da.capacity];
+	for (int i = 0; i < da.capacity / 2; i++)
+	{
+		da.data[i] = oldData[i];
+	}
+	delete[] oldData;
+}
+
+void PushBack(DynArray& da, int element) {
+	if (da.size == da.capacity)
+	{
+		Resize(da);
+	}
+	da.data[da.size++] = element;
+}
+
+void PopBack(DynArray& da) {
+	da.size--;
+}
+
+void Remove(DynArray& da, int index) {
+	assert(index >= 0 && index < da.size);
+	for (int i = index; i < da.size - 1; ++i)
+	{
+		da.data[i] = da.data[i + 1];
+	}
+	da.size--;
+}
+
+int Get(const DynArray& da, int index) {
+	assert(index >= 0 && index < da.size);
+	return da.data[index];
+}
+
+void Print(const DynArray& da) {
+	cout << "DynArray: \n";
+	cout << "\tsize: " << da.size << endl;
+	cout << "\telements: \n";
+	for (int i = 0; i < da.size; ++i)
+	{
+		cout << "\t\t" << Get(da, i) << endl;
+	}
+}
+
+int main()
+{
+	DynArray da = Create(5);
+	PushBack(da, 2);
+	PushBack(da, 3);
+	PushBack(da, 4);
+	PushBack(da, 22);
+	PushBack(da, 3345);
+	PushBack(da, 434);
+	Print(da);
+	Remove(da, 0);
+	PopBack(da);
+	Print(da);
+
+
+
+
+}
